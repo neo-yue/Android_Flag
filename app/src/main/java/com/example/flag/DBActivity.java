50e6d58016b7c683bff3aa3ba43d9b14e04a56a9
w0403665 <71242653+w0403665@users.noreply.github.com>
2021-12-07 18:04:10 -0400

    dbpart1

--- app/src/main/java/com/example/flag/DBActivity.java
+++ app/src/main/java/com/example/flag/DBActivity.java
@@ -1,9 +1,14 @@
 package com.example.flag;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.*;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.*;
+import android.database.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class DBActivity extends AppCompatActivity {
 
@@ -12,10 +17,164 @@ public class DBActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dbactivity);
 
-
-//        TextView title;
-//        title=findViewById(R.id.title);
-
         Bundle extras = getIntent().getExtras();
+
+        List<String> flags = Arrays.asList(getResources().getStringArray(R.array.countries_name));
+
+        try{
+
+            String destPath = "/data/data/" + getPackageName() +"/database/MyDB";
+
+            File f = new File(destPath);
+
+            if(!f.exists()){
+
+                CopyDB(getBaseContext().getAssets().open("mydb"),
+
+                        new FileOutputStream(destPath));
+
+            }
+
+
+        }catch (FileNotFoundException e){
+
+            e.printStackTrace();
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
+
+
+        DBAdapter db = new DBAdapter(this);
+
+
+//add a contact- CREATE
+
+        db.open();
+        long id;
+        for (int i=0;i<flags.size();i++){
+            id = db.insertRate(flags.get(i),"",10);
+
+        }
+        db.close();
+
+
+//get all contacts - READ
+
+        db.open();
+
+        Cursor c = db.getAllRate();
+
+        if(c.moveToFirst())
+
+        {
+
+            do{
+
+                DisplayContact(c);
+
+            }while(c.moveToNext());
+
+        }
+
+        db.close();
+
+
+//get a single contact - READ
+
+        db.open();
+
+        c = db.getRate(2);
+
+        if(c.moveToFirst())
+
+            DisplayContact(c);
+
+        else
+
+            Toast.makeText(this,"No contact found",Toast.LENGTH_LONG).show();
+
+
+        db.close();
+
+
+//update a contact - UPDATE
+
+        db.open();
+
+        if(db.updateRate(1,"China","the best",10))
+
+            Toast.makeText(this,"Update successful",Toast.LENGTH_LONG).show();
+
+        else
+
+            Toast.makeText(this,"Update failed",Toast.LENGTH_LONG).show();
+
+        db.close();
+
+
+//delete a contact - DELETE
+
+        db.open();
+
+        if(db.deleteRate(1))
+
+            Toast.makeText(this,"Delete successful",Toast.LENGTH_LONG).show();
+
+        else
+
+            Toast.makeText(this,"Delete failed",Toast.LENGTH_LONG).show();
+
+
+        db.close();
+
+
+    }//end method onCreate
+
+
+    public void CopyDB(InputStream inputStream,OutputStream outputStream)
+
+            throws IOException{
+
+//copy 1k bytes at a time
+
+        byte[] buffer = new byte[1024];
+
+        int length;
+
+        while((length = inputStream.read(buffer)) > 0)
+
+        {
+
+            outputStream.write(buffer,0,length);
+
+        }
+
+        inputStream.close();
+
+        outputStream.close();
+
+
+    }//end method CopyDB
+
+
+    public void DisplayContact(Cursor c)
+
+    {
+
+        Toast.makeText(this,
+
+                "id: " + c.getString(0) + "\n" +
+
+                        "Name: " + c.getString(1) + "\n" +
+                        "Rate: " + c.getFloat(2) + "\n" +
+                        "common: " + c.getString(3),
+
+                Toast.LENGTH_LONG).show();
+
     }
-}
+
+
+}//end class
---
+++ app/src/main/java/com/example/flag/DBAdapter.java
@@ -0,0 +1,154 @@
+package com.example.flag;
+
+import android.content.*;
+import android.database.*;
+import android.database.sqlite.*;
+import android.util.Log;
+
+public class DBAdapter {
+    public static final String rowID = "id";
+    public static final String flagName = "name";
+    public static final String rate = "rate";
+    public static final String comment = "comment";
+    public static final String TAG = "DBAdapter";
+    public static final String DATABASE_NAME = "MyDB";
+    public static final String DATABASE_TABLE = "Rating";
+    public static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table Rating(id integer primary key autoincrement," +
+            "name text not null,rate float not null,comment text not null)";
+
+    private Context context = null;
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+
+    public DBAdapter(Context ctx) {
+
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        }
+
+
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(DATABASE_CREATE);
+
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+
+            }
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+            Log.w(TAG, "Upgrade database from version " + oldVersion + " to "
+
+                    + newVersion + ", which will destroy all old data");
+
+            db.execSQL("DROP TABLE IF EXISTS Rating");
+
+            onCreate(db);
+
+        }//end method onUpgrade
+
+    }
+//open the database
+
+    public DBAdapter open() throws SQLException {
+
+        db = DBHelper.getWritableDatabase();
+
+        return this;
+
+    }
+
+
+//close the database
+
+    public void close() {
+
+        DBHelper.close();
+
+    }
+
+
+//insert a contact into the database
+
+    public long insertRate(String name, String com, float rateScore) {
+
+        ContentValues initialValues = new ContentValues();
+
+        initialValues.put(flagName, name);
+        initialValues.put(rate, rateScore);
+        initialValues.put(comment, com);
+
+        return db.insert(DATABASE_TABLE, null, initialValues);
+
+    }
+
+
+//delete a particular contact
+
+    public boolean deleteRate(long rowId) {
+
+        return db.delete(DATABASE_TABLE, rowID + "=" + rowId, null) > 0;
+
+    }
+
+
+//retrieve all the contacts
+
+    public Cursor getAllRate() {
+
+        return db.query(DATABASE_TABLE, new String[]{rowID, flagName,
+
+                rate, comment}, null, null, null, null, null);
+
+    }
+
+
+//retrieve a single contact
+
+    public Cursor getRate(long rowId) throws SQLException {
+
+        Cursor mCursor = db.query(true, DATABASE_TABLE, new String[]{rowID, flagName,
+
+                rate, comment}, rowID + "=" + rowId, null, null, null, null, null);
+
+        if (mCursor != null) {
+
+            mCursor.moveToFirst();
+
+        }
+
+        return mCursor;
+
+    }
+
+
+//updates a contact
+
+    public boolean updateRate(long rowId, String name, String com, float rateScore) {
+
+        ContentValues cval = new ContentValues();
+
+        cval.put(flagName, name);
+        cval.put(comment, com);
+        cval.put(rate, rateScore);
+
+
+        return db.update(DATABASE_TABLE, cval, rowID + "=" + rowId, null) > 0;
+
+    }
+}
+
